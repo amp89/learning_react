41ab42e2cb4f52cb068b5fdf8c252d13417c47de
Alex Peterson <alexander.peterson89@gmail.com>
2017-06-17 14:50:08 -0400

    return react components, first treehouse lesson complete

--- treehouse_notes/basics/code/app.jsx
+++ treehouse_notes/basics/code/app.jsx
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello!!!!</h1>,document.getElementById("container"));
+
+function Application(){
+    return (
+        <div>
+            <h1>Helloooooo!</h1>
+            <p>Rendered by Application Componenet</p>
+        </div>
+    );
+
+}
+
+//ReactDOM.render(<h1>Hello!!!!</h1>,document.getElementById("container"));
+ReactDOM.render(<Application />, document.getElementById('container'));
