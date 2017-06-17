6d6b5cd5eb7f08f95270f9f6850553ad10a775f9
Alex Peterson <alexander.peterson89@gmail.com>
2017-06-17 15:38:40 -0400

    subcomponents, next looping

--- treehouse_notes/basics/code/app.jsx
+++ treehouse_notes/basics/code/app.jsx
@@ -1,50 +1,68 @@
+function Header(props){
+    return (
+        <div className="header">
+            <h1>
+                {props.title}
+            </h1>
+        </div>
+    )
+
+}
+
+Header.propTypes = {
+
+    title:React.PropTypes.string.isRequired //No default value on this one, so make required.
+
+}
+
+function Counter(props){
+    return (
+        <div className="counter">
+            <button className="counter-action decrement">-</button>
+            <button className="counter-score">{props.score}</button>
+            <button className="counter-action increment">+</button>
+        </div>
+    )
+
+}
+
+Counter.propTypes = {
+    score:React.PropTypes.number.isRequired //No default value on this one, so make required.
+}
+
+
+
+function Player(props){
+
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <div className="player-score">
+                <Counter score={props.score} />
+            </div>
+        </div>
+
+    )
+
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+}
+
 
 function Application(props){ //props.title=My Scoreboard
     return (
         <div className="scoreboard">
-            <div className="header">
-                <h1>
-                    {props.title}
-                </h1>
-            </div>
+            <Header title={props.title} />
 
             <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Alex Peterson
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement">-</button>
-                            <button className="counter-score">99</button>
-                            <button className="counter-action increment">+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="player">
-                    <div className="player-name">
-                        Alex Peterson
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement">-</button>
-                            <button className="counter-score">99</button>
-                            <button className="counter-action increment">+</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="player">
-                    <div className="player-name">
-                        Alex Peterson
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement">-</button>
-                            <button className="counter-score">99</button>
-                            <button className="counter-action increment">+</button>
-                        </div>
-                    </div>
-                </div>
+                <Player name="Alex Peterson" score={654} />
+                <Player name="Alex Peterson" score={65} />
+                <Player name="Alex Peterson" score={54} />
             </div>
 
         </div>
