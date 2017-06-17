b49881d37192b30921c70978f040f73edfd73867
Alex Peterson <alexander.peterson89@gmail.com>
2017-06-17 15:16:48 -0400

    componenets prop types required default passing numbers

--- treehouse_notes/basics/code/app.jsx
+++ treehouse_notes/basics/code/app.jsx
@@ -1,13 +1,70 @@
 
-function Application(){
+function Application(props){ //props.title=My Scoreboard
     return (
-        <div>
-            <h1>Helloooooo!</h1>
-            <p>Rendered by Application Componenet</p>
+        <div className="scoreboard">
+            <div className="header">
+                <h1>
+                    {props.title}
+                </h1>
+            </div>
+
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                        Alex Peterson
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement">-</button>
+                            <button className="counter-score">99</button>
+                            <button className="counter-action increment">+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="player">
+                    <div className="player-name">
+                        Alex Peterson
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement">-</button>
+                            <button className="counter-score">99</button>
+                            <button className="counter-action increment">+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="player">
+                    <div className="player-name">
+                        Alex Peterson
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement">-</button>
+                            <button className="counter-score">99</button>
+                            <button className="counter-action increment">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 
 }
 
+Application.propTypes = {
+
+    title:React.PropTypes.string.isRequired
+
+}
+
+Application.defaultProps = {
+    title: "Untitled Scoreboard"
+}
+
+
 //ReactDOM.render(<h1>Hello!!!!</h1>,document.getElementById("container"));
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(
+        <Application />,
+        document.getElementById('container'));//since proptype string, this will cause an error
+// ReactDOM.render(<Application title="MY SCOREBOARD"/>, document.getElementById('container'));
