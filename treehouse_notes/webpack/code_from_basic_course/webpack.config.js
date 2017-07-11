4827325440072b5e3b14baa1e7cd174dc331d6e7
Alex Peterson <alexander.peterson89@gmail.com>
2017-07-11 00:24:04 -0400

    updated webpack to include build script

--- treehouse_notes/webpack/code_from_basic_course/webpack.config.js
+++ treehouse_notes/webpack/code_from_basic_course/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx$/,
+                test: /\.jsx$/, //had to make this .jsx instead of .js
                 exclude: /node_modules/,
                 loader: 'babel-loader'
             }
